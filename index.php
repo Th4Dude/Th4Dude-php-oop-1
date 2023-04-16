a85745a7015bb6e1f73a0134724701060ebd00e6
Th4Dude <gallottimaurizio@yahoo.it>
2023-04-16 14:46:50 +0300

    classe

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// classe Movie
+class Movie {
+    public $titolo;
+    public $regista;
+    public $genere;
+    public $anno; 
+    public $durata;      // variabili d'istanza
+}
+
+
+
+
+
+
+?>
