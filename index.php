384970dc363545986d126e3a8803659b2a752a32
Th4Dude <gallottimaurizio@yahoo.it>
2023-04-16 15:45:42 +0300

    Finito

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ class Movie {
     public $durata;      // variabili d'istanza
 
     // costruttore
-    public function __construct($regista,$titolo,  $genere, $durata,  $anno,) {
+    public function __construct($regista,$titolo, $genere, $durata, $anno,) {
         $this->titolo = $titolo;
         $this->regista = $regista;
         $this->genere = $genere;
@@ -19,17 +19,18 @@ class Movie {
 
     // metodo
     public function info() {
-        return "Con Regista {$this->regista} esce '{$this->titolo}' dal genere {$this->genere} della durata di '{$this->durata}' nell'anno{$this->anno} .";
+        return "Con Regista {$this->regista} esce '{$this->titolo}' dal genere {$this->genere} della durata di '{$this->durata}' nell'anno {$this->anno} .";
     }
 
 }
 
+$firstMovie = new Movie("Michele Placido","Romanzo Criminale","Gangster,drammatico", 153, 2005 );
+$secondMovie = new Movie("Ridley Scott","Il Gladiatore", "drammatico", 155, 2000 );
+$thirdMovie = new Movie("Quentin Tarantino","Django", "Western", 165, 2012);
 
-
-
-
-
-
-
+// riporto a schermo
+echo $firstMovie->info() . "<br>";
+echo $secondMovie->info() . "<br>";
+echo $thirdMovie->info() . "<br>";
 
 ?>
