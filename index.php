019794fd2b44be3d1ed2e40a2954de0ef5502c35
Th4Dude <gallottimaurizio@yahoo.it>
2023-04-16 15:14:20 +0300

    costruttore

--- index.php
+++ index.php
@@ -7,6 +7,24 @@ class Movie {
     public $genere;
     public $anno; 
     public $durata;      // variabili d'istanza
+
+    // costruttore
+    public function __construct($titolo, $regista, $genere, $anno, $durata) {
+        $this->titolo = $titolo;
+        $this->regista = $regista;
+        $this->genere = $genere;
+        $this->anno = $anno;
+        $this->durata = $durata;
+    }
+
+
+
+
+
+
+
+
+
 }
 
 
